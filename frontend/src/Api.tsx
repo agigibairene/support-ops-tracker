601d5cef8420e83761bd7ce347bb1659fb00f3e3
agigibairene <i.agigiba@alustudent.com>
2026-09-14 05:46:15 +0000

    added baseUrl

--- frontend/src/Api.tsx
+++ frontend/src/Api.tsx
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://127.0.0.1:8000';
+export const baseUrl = 'https://support-ops-tracker-production.up.railway.app';
 import axios from "axios";
 import type { Activity, ActivityUpdate } from "./types";
 
